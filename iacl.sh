df7d47cc8833341f20d6771c9392c438ee0d2065
Samuel Renê <110557521+SamRGS@users.noreply.github.com>
2022-08-11 10:38:44 -0300

    Update iacl.sh

--- iacl.sh
+++ iacl.sh
@@ -23,7 +23,7 @@ useradd debora  -m -s /bin/bash  -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 useradd sebastiana  -m -s /bin/bash  -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 useradd roberto  -m -s /bin/bash  -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 
-useraddajosefina  -m -s /bin/bash  -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd josefina  -m -s /bin/bash  -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd amanda  -m -s /bin/bash  -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd rogerio  -m -s /bin/bash  -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 
